9d86bca233a4ea110e8f72fb57bd0af269c90ddb
Abdullah al masud <abdullahalmasud01737906772@gmail.com>
2024-12-07 00:40:55 +0600

    compleate graphql this project module-part-3 2

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,9 +1,13 @@
+import { userLoader } from "../dataLoaders/userLoader";
+
 export const Post = {
   author: async (parent: any, args: any, { prisma, userInfo }: any) => {
-    return await prisma.user.findUnique({
-      where: {
-        id: parent.authorId,
-      },
-    });
+    console.log("post", parent.authorId);
+    return userLoader.load(parent.authorId);
   },
 };
+
+// await prisma.user.findUnique({
+//   where: {
+//     id: parent.authorId,
+//   },
